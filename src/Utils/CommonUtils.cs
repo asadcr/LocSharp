6de5a252ce25d90c16d3537115a6bbaffc3285c1
Asad Ali <asadcr@live.com>
2020-03-03 11:59:36 +0500

    efficient extension detection

--- src/Utils/CommonUtils.cs
+++ src/Utils/CommonUtils.cs
@@ -1,10 +1,28 @@
 ﻿namespace LocSharp.Utils
 {
     using System.IO;
+    using System.Linq;
     using System.Reflection;
 
     public static class CommonUtils
     {
+        private const char DirectorySeparatorChar = '/';
+        private const char AltDirectorySeparatorChar = '\\';
+
         public static string BasePath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static string NormalizePath(string path)
+        {
+            Arg.NotNull(path, nameof(path));
+
+            return path.Replace(AltDirectorySeparatorChar, DirectorySeparatorChar);
+        }
+
+        public static string GetFileName(string path)
+        {
+            Arg.NotNull(path, nameof(path));
+
+            return NormalizePath(path).TrimEnd(DirectorySeparatorChar).Split(DirectorySeparatorChar).Last();
+        }
     }
 }
